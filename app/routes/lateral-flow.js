dc6285571a0f193b3da2ad51ca328295e9afb6a7
Henry Cookson <henry.cookson@rocketmail.com>
2021-07-21 13:09:19 +0100

    add link from share-lateral-flow-result to nhsuk-frotnend version. Also add jump off point options

--- app/routes/lateral-flow.js
+++ app/routes/lateral-flow.js
@@ -2,23 +2,31 @@ const express = require('express');
 const router = express.Router();
 var moment = require('moment');
 
-// Taken test for job?
+// Taken test for job? - NHS styles
 
-router.post('/app/share-result-lateral-flow/did-you-take-this-test-for-your-job', function (req, res) {
-  let answer = req.body.takeTestForJob;
+router.post('/app/share-result-lateral-flow/nhs-styles/did-you-take-this-test-for-your-job-nhs', function (req, res) {
+  let answer = req.body.takeTestForJobNHS;
   if (answer === 'Yes') {
-    res.redirect('/app/share-result-lateral-flow/about-work')
+    res.redirect('/app/share-result-lateral-flow/nhs-styles/about-work')
+  }
+  if (answer === 'No') {
+    res.redirect('/app/share-result-lateral-flow/nhs-styles/test-reason')
   } else {
-    res.redirect('/app/share-result-lateral-flow/test-reason')
+    res.redirect('/app/share-result-lateral-flow/nhs-styles/did-you-take-this-test-for-your-job-nhs')
   }
 });
+
+// Taken test for job? - GOV styles
 
 router.post('/app/share-result-lateral-flow/did-you-take-this-test-for-your-job-gov', function (req, res) {
   let answer = req.body.takeTestForJob;
   if (answer === 'Yes') {
     res.redirect('/app/share-result-lateral-flow/about-work')
-  } else {
+  }
+  if (answer === 'No') {
     res.redirect('/app/share-result-lateral-flow/test-reason')
+  } else {
+    res.redirect('/app/share-result-lateral-flow/did-you-take-this-test-for-your-job-gov')
   }
 });
 
