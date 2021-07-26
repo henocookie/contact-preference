79598db7e537fe89987ab5719da2f4fb5eaa6779
Henry Cookson <henry.cookson@rocketmail.com>
2021-07-26 11:36:07 +0100

    add employee wtp screen for non social care, education or NHS work

--- app/routes/lateral-flow.js
+++ app/routes/lateral-flow.js
@@ -30,17 +30,37 @@ router.post('/app/share-result-lateral-flow/did-you-take-this-test-for-your-job-
   }
 });
 
+// About work - GOV styles
+
+router.post('/app/share-result-lateral-flow/about-work', function (req, res) {
+  let answer = req.body.aboutWork;
+  if (answer === 'It’s for a social care service') {
+    res.redirect('/app/share-result-lateral-flow/test-date')
+  }
+  if (answer === 'It’s for an education provider') {
+    res.redirect('/app/share-result-lateral-flow/test-date')
+  }
+  if (answer === 'It’s for the NHS in England, Northern Ireland or Wales') {
+    res.redirect('/app/share-result-lateral-flow/test-date')
+  }
+  if (answer === 'It’s not listed') {
+    res.redirect('/app/share-result-lateral-flow/employee-wtp')
+  } else {
+    res.redirect('/app/share-result-lateral-flow/about-work')
+  }
+});
+
 // Test result - previously reported a result journey
 
 router.post('/app/share-result-lateral-flow/choose-result', function (req, res) {
-  let testResultAnswer = req.body.testResult;
-  if (testResultAnswer === 'check-answers-positive') {
+  let answer = req.body.testResult;
+  if (answer === 'check-answers-positive') {
     res.redirect('/app/share-result-lateral-flow/positive-result')
   }
-  if (testResultAnswer === 'check-answers-negative') {
+  if (answer === 'check-answers-negative') {
     res.redirect('/app/share-result-lateral-flow/negative-result')
   }
-  if (testResultAnswer === 'check-answers-invalid') {
+  if (answer === 'check-answers-invalid') {
     res.redirect('/app/share-result-lateral-flow/invalid-result')
   } else {
     res.redirect('/app/share-result-lateral-flow/choose-result')
